404bbdf51724f7165c87bf557aa5c7257d339212
Estelle van-anh beverley EGANA <estegana@bess-f2r4s11.clusters.42paris.fr>
2025-01-24 17:07:34 +0100

    surcharge op aff global et interne (=)

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -1,5 +1,7 @@
 #include "Fixed.hpp"
 
+// Fixed::_bits = 8;
+
 //constructeur par default
 Fixed::Fixed()
 {
@@ -7,33 +9,51 @@ Fixed::Fixed()
 }
 
 //constructeur de int
-Fixed::Fixed( const int nb ) 
+Fixed::Fixed( const int nb ) : _nb(nb)
 {
     std::cout << "Int constructor called" << std::endl;
 }
 
 //constructeur de float
-Fixed::Fixed( const float nb )
+Fixed::Fixed( const float nb ) 
 {
     std::cout << "Float constructor called" << std::endl;
 }
 
-//constructeur de recopie
+//constructeur de recopie (duplique une classe)
 Fixed::Fixed ( const Fixed &autre )
 {
     std::cout << "Copy constructor called" << std::endl;
     *this = autre;
 }
 
-//operateur d'affectation
-// Fixed &Fixed::operator=( const Fixed &autre )
-// {
-//     std::cout << "Copy assignment operator called" << std::endl;
-//     return (*this);
-// }
+//operateur d'affectation (affecte a une classe initialisee a null des valeurs)
+Fixed &Fixed::operator=( const Fixed &autre )
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->_nb = autre._nb;
+    return (*this);
+}
 
 //destructeur
 Fixed::~Fixed()
 {
     std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::toInt( void ) const
+{
+    return (this->_nb);
+}
+
+int Fixed::toFloat( void ) const
+{
+    
+}
+
+//surcharge doperateur global
+std::ostream &operator<<(std::ostream &o, Fixed const &i)
+{
+    o << i.toInt();
+    return (o);
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,22 +6,24 @@
 class Fixed
 {
     private:
-       
-        static const int _bits;
+        int _nb;
+        static const int _bits;//initialise a 8
 
     public:
-        int _nb;
+        
         Fixed();//constructeur par defaut
         Fixed( const int nb );//constructeur de int
         Fixed( const float nb );//constructeur de float
         Fixed( const Fixed &autre );//constructeur de recopie
-        // Fixed &operator=( const Fixed &autre );//surcharge de l'operateur d'affectation
+        Fixed &operator=( const Fixed &autre );//surcharge de l'operateur d'affectation
         ~Fixed();
 
     // float toFloat( void ) const;
-    // int toInt( void ) const;
+    int toInt( void ) const;
 };
 
+//surcharge d'operateur global
+std::ostream &operator<<(std::ostream &o, Fixed const &i);
 
 
 #endif
