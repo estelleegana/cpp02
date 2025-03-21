cae598ab83defecc66b50fb580625695cfb3b3e6
Estelle van-anh beverley EGANA <estegana@bess-f3r6s3.clusters.42paris.fr>
2025-03-21 13:39:26 +0100

    done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -8,22 +8,35 @@ int main( void )
     Fixed y(5.05f);
     Fixed z(2);
 
-    std::cout << bGREEN << "                  incr/decr" << RESET << std::endl;  
-    std::cout << bold << "                  ++" << RESET << std::endl;    
+    std::cout << bGREEN << "           ____ incr/decr ____ " << RESET << std::endl;  
+    std::cout << bold << "++" << RESET << std::endl;    
     std::cout << "a : " << a << std::endl;
     std::cout << "++a : " << ++a << std::endl;
     std::cout << "a : " << a << std::endl;
     std::cout << "a++ : " << a++ << std::endl;
     std::cout << "a : " << a << std::endl << std::endl;
 
-    std::cout << bold << "                  --" << RESET << std::endl; 
+    std::cout << bold << "--" << RESET << std::endl; 
     a.setRawBits(0);
     std::cout << "a : " << a << std::endl;
     std::cout << "--a : " << --a << std::endl;
     std::cout << "a : " << a << std::endl;
-    std::cout << "a-- : " << a-- << std::endl << std::endl;
+    std::cout << "a-- : " << a-- << std::endl;
+    std::cout << "a : " << a << std::endl << std::endl;
 
-    std::cout << bGREEN << "                  comparaisons" << RESET << std::endl; 
+    std::cout << bGREEN << "           ____ arithmetic ____ " << RESET << std::endl;
+    std::cout << "x : " << x << std::endl;
+    std::cout << "y : " << y << std::endl;  
+    std::cout << bold << "+" << RESET << std::endl;
+    std::cout << "x + y = " << (x + y) << std::endl << std::endl;
+    std::cout << bold << "*" << RESET << std::endl;
+    std::cout << "x * y = " << (x * y) << std::endl << std::endl;
+    std::cout << bold << "-" << RESET << std::endl;
+    std::cout << "x - y = " << (x - y) << std::endl << std::endl;
+    std::cout << bold << "/" << RESET << std::endl;
+    std::cout << "x / y = " << (x / y) << std::endl << std::endl;
+
+    std::cout << bGREEN << "           ____ comparaisons ____ " << RESET << std::endl; 
     std::cout << "x : " << x << std::endl;
     std::cout << "y : " << y << std::endl;
     std::cout << "z : " << z << std::endl;
@@ -36,12 +49,13 @@ int main( void )
     std::cout << "x != z : " << (x != z) << std::endl;
     std::cout << "x == z : " << (x == z) << std::endl << std::endl;
 
-    std::cout << bGREEN << "                  max / min" << RESET << std::endl;
+    std::cout << bGREEN << "           ____ max / min ____ " << RESET << std::endl;
+    std::cout << "a : " << a << std::endl;
     std::cout << "b : " << b << std::endl;
-    std::cout << bold << "                  max" << RESET << std::endl;  
-    // std::cout << "MAXXXX " << std::max(a, b) << std::endl;
+    std::cout << bold << "max" << RESET << std::endl;  
+    // std::cout << "std::max(a, b) : " << std::max(a, b) << std::endl;
     std::cout << "Fixed::max(a, b) : " << Fixed::max(a, b) << std::endl;
-    std::cout << bold << "                  min" << RESET << std::endl; 
+    std::cout << bold << "min" << RESET << std::endl; 
     std::cout << "Fixed::min(a, b) : " << Fixed::min(a, b) << std::endl;
 
 
